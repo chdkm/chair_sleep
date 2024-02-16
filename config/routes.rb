8d4f71f2c455e3be960a515327a88d29fbf6bd86
金　志大 <kimuchide@MBPro.local>
2024-02-17 03:24:48 +0900

    ブックマーク機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :posts, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create edit destroy], shallow: true
+    collection do
+      get :bookmarks
+    end
   end
   resource :profile, only: %i[show edit update]
+  resources :bookmarks, only: %i[create destroy]
 end
