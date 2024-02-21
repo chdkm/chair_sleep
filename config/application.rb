e4f33d5a3c4c6f707263c891d1762882191d0d0f
金　志大 <kimuchide@MBPro.local>
2024-02-21 22:28:25 +0900

    conflict_fix

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 
-require 'dotenv/rails-now'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-Dotenv::Railtie.load
 
 module App
   class Application < Rails::Application
