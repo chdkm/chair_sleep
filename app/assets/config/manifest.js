f03b8fa3757d65d220787c18e3c43192a714c331
金　志大 <kimuchide@MBPro.local>
2024-06-30 19:33:34 +0900

    アセットパイプライン修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,6 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link application.tailwind.css
 //= link_tree ../../../vendor/javascript .js
-//= link_directory ../stylesheets .css
 //= link controllers/application.js
 //= link controllers/hello_controller.js
 //= link controllers/index.js
